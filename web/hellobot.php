74659a7e3df457803f103659f5bea2ab941ce930
ileasile <leopardile@yandex.ru>
2016-08-30 00:34:20 +0300

    fix5

--- web/hellobot.php
+++ web/hellobot.php
@@ -126,8 +126,8 @@ function processMessage($message) {
     $text = $message['text'];
 
     if (strpos($text, "/start") === 0) {
-      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Добрый день! Кто Вы?', 'reply_markup' => array(
-        'keyboard' => array(array('Преподаватель', 'Бакалавр', 'Магистр')),
+      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Hey, who are you?', 'reply_markup' => array(
+        'keyboard' => array(array('t', 'b', 'm')),
         'one_time_keyboard' => true,
         'resize_keyboard' => true)));
     } 
