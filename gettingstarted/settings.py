49abecfe1147cd0bf143abc83da01ecf912cb7c1
ileasile <leopardile@yandex.ru>
2016-08-30 13:41:41 +0300

    try1

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'hello',
+	'teleport'
 )
 
 MIDDLEWARE_CLASSES = (
