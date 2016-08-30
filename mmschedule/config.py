00fa03db11746fb535238da48bf547f9f11d607d
ileasile <leopardile@yandex.ru>
2016-08-30 13:21:35 +0300

    com9

--- mmschedule/config.py
+++ mmschedule/config.py
@@ -8,5 +8,5 @@ WEBHOOK_HOST = 'mmschedule.herokuapp.com/mmschedule'
 #WEBHOOK_PORT = 80  # 443, 80, 88 или 8443 (порт должен быть открыт!)
 #WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
 
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (config.token)
+#WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+#WEBHOOK_URL_PATH = "/%s/" % (config.token)
