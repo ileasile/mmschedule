3b6fbbc854fa9de04985200ac9618f349a07839d
ileasile <leopardile@yandex.ru>
2016-08-31 14:38:19 +0300

    try4

--- mmschedule/bot.py
+++ mmschedule/bot.py
@@ -7,16 +7,19 @@ from django.http import HttpResponse
 
 bot = telebot.TeleBot(config.token)
 def process_request(req):
-	return HttpResponse(str(req.META))
-	if req.req.META['CONTENT_TYPE'] == 'application/json':
-		length = int(req.META['CONTENT_LENGTH'])
-		json_string = req.read(length).decode("utf-8")
-		update = telebot.types.Update.de_json(json_string)
-		# Эта функция обеспечивает проверку входящего сообщения
-		bot.process_new_updates([update])
-		return HttpResponse('')
-	else:
-		return HttpResponse('NOT json: ')
+	#return HttpResponse(str(req.META))
+	try:
+		if req.req.META['CONTENT_TYPE'] == 'application/json':
+			length = int(req.META['CONTENT_LENGTH'])
+			json_string = req.read(length).decode("utf-8")
+			update = telebot.types.Update.de_json(json_string)
+			# Эта функция обеспечивает проверку входящего сообщения
+			bot.process_new_updates([update])
+			return HttpResponse('')
+		else:
+			return HttpResponse('NOT json: ')
+	except:
+		return JsonResponse({'ok':False, 'description': 'Sth gone wrong'})
 	
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
