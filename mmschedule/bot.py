2fe9dc5476aeca53c297564d1fa1814d884ec087
ileasile <leopardile@yandex.ru>
2016-09-01 08:08:36 +0300

    FIRST NORMAL CHANGE

--- mmschedule/bot.py
+++ mmschedule/bot.py
@@ -15,7 +15,7 @@ def process_request(req):
 			length = int(req.META['CONTENT_LENGTH'])
 			json_string = req.read(length).decode("utf-8")
 			update = telebot.types.Update.de_json(json_string)
-			print('i\'m here')
+			print('Got response from Telegram')
 			# Эта функция обеспечивает проверку входящего сообщения
 			bot.process_new_updates([update])
 			return HttpResponse('')
@@ -23,7 +23,31 @@ def process_request(req):
 			return HttpResponse('NOT json: ')
 	except:
 		return JsonResponse({'ok':False, 'description': 'Sth gone wrong'})
+
+class DataBase:
+        def DataBase(self, filename):
+                self.filename = filename
+                f = open(filename, 'r')
+                self.data = map(lambda s: map(lambda x: x.strip(), s.split("|")), list(f))
+                f.close()
+        def pack(self):
+                f = open(self.filename, 'w')
+                f.write("\n".join(map(lambda row: "|".join(row), self.data)))
+                f.close()
+                
+class DataBaseDict:
+        def DataBaseDict(self, filename):
+                self.filename = filename
+                f = open(filename, 'r')
+                self.data = reduce(lambda x, y: x + {y.split("|")[0] : y.split("|")[1,]}, list(f), {})
+                f.close()
+        def pack(self):
+                f = open(self.filename, 'w')
+                f.write("\n".join(map(lambda row: "|".join(row[0] + row[1]), self.data.items())))
+                f.close()
 	
-@bot.message_handler(func=lambda message: True, content_types=['text'])
+	
+@bot.message_handler(commands=['start'])
 def echo_message(message):
-	bot.reply_to(message, message.text)
+	human_id = message.from_user.id
+	bot.reply_to(message, map(lambda x: x[0], DataBaseDict(BOT_TEACHERS_DB).data.values()))
